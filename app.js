301d79118f5fee1f8cf5e2a168463322478cc148
Arfan Uddin <arfan.rfn@gmail.com>
2019-04-01 22:51:17 -0700

    added google auth with cookie setup

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mysql = require('mysql');
+const passport = require('passport');
+const config = require('./config');
+const cookieSession = require('cookie-session');
 
 const app = express();
 var connection = mysql.createConnection({
@@ -10,6 +13,58 @@ var connection = mysql.createConnection({
     timeout: 60000
 });
 
+app.use(cookieSession({
+    maxAge: 5 * 24 * 60 * 60 * 1000, // 5 day for cookie
+    keys: [config.cookieKey],
+}));
+
+// init passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// store user serialize data in browser cookie 
+passport.serializeUser((currUser, cb) => {
+    // TODO: user = user row in the db, and currUser.id is the whatever id for the user
+    cb(null, currUser.sub);
+});
+
+// get user data from browser cookie 
+passport.deserializeUser((id, cb) => {
+    var currUser = id; // user after check/create in the db
+    // TODO: query on the db based on the id (user table id), store back to currUser var
+    console.log('id of the user from cookie: ' + id);
+    cb(null, currUser);
+});
+
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+
+passport.use(new GoogleStrategy({
+        clientID: config.google_client_id,
+        clientSecret: config.google_client_secret,
+        callbackURL: " /auth/google/redirect"
+    },
+    function (accessToken, refreshToken, profile, cb) {
+        var currUser = profile._json; // user after check/create in the db
+
+        // TODO: check the user table if any user with this id "profile._json.sub" exist,
+        //  if yes, then just login the user
+        //  else, create a new user with the profile._json data. 
+        /**
+        profile._json = { sub: '101216936986399344837',
+        name: 'Wilbur Wildcat',
+        given_name: 'Wilbur',
+        family_name: 'Wildcat',
+        profile: 'https://plus.google.com/101216936986399344837',
+        picture: 'https://lh6.googleusercontent.com/-6UdZWLEP7oE/AAAAAAAAAAI/AAAAAAAAAAA/ACHi3rdyawnB0iISiYbR0rHOMEojDL3_VQ/mo/photo.jpg',
+        gender: 'male',
+        locale: 'en' }
+         */
+        console.log(profile._json);
+        // TODO: after user login/signup, call cb(null, currUser) user is the user profile
+        cb(null, currUser);
+    }
+));
+
 connection.connect(function (err) {
     if (err) {
         console.error('error connecting: ' + err.stack);
@@ -19,7 +74,23 @@ connection.connect(function (err) {
 });
 
 app.get('/', (req, res) => {
-    res.send("hello world");
+    if (req.user) {
+        res.send('auth user');
+    } else {
+        res.send("guest user");
+    }
+});
+
+// redirect to google login page
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile']
+}));
+
+// callback route for google to redirect to
+app.get('/auth/google/redirect', passport.authenticate('google'), (req, res) => {
+    // req.user will give the curr user obj
+    console.log(req.user);
+    res.redirect('/');
 });
 
 app.listen(3000, () => {
---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "google_client_id": "394263195270-7mq7tb6lgl0rjqrjsganc5it3vqcvul8.apps.googleusercontent.com",
+    "google_project_id": "mykitchen-236301",
+    "google_auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "google_token_uri": "https://oauth2.googleapis.com/token",
+    "google_auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "google_client_secret": "87QdxXNsbkZTrvYgFfKl5vrv",
+    "google_redirect_uris": ["http://localhost:3000"],
+    "cookieKey": "thisIsTheCookieKey",
+};
