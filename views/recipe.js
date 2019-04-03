41aa0426d5d7cf9f7e9a7ba6706abe1eac5ff907
rubbalkumar <rubbalkumar@email.arizona.edu>
2019-04-02 20:19:35 -0700

    adfa

---
+++ views/recipe.js
@@ -0,0 +1,30 @@
+function openNav2() {
+  document.getElementById("mySidenav2").style.width = "250px";
+  document.getElementById("main").style.marginRight = "250px";
+  document.getElementById('shoppingLst').style.display = 'none';
+}
+
+function closeNav2() {
+  document.getElementById("mySidenav2").style.width = "0";
+  document.getElementById("main").style.marginRight = "0";
+  document.getElementById('shoppingLst').style.display = '';
+}
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById('pantryLst').style.display = 'none';
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+  document.getElementById('pantryLst').style.display = '';
+}
+
+
+
+
+
+
+
+
