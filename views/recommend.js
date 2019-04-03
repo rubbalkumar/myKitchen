e8a6bff4ee8c47e015f1c8643b4594966496445e
rubbalkumar <rubbalkumar@email.arizona.edu>
2019-04-03 14:22:59 -0700

    html

---
+++ views/recommend.js
@@ -0,0 +1,31 @@
+function openNav2() {
+    document.getElementById("mySidenav2").style.width = "250px";
+    document.getElementById("main").style.marginRight = "250px";
+    document.getElementById('shoppingLstNavBar').style.display = 'none';
+  }
+  
+  function closeNav2() {
+    document.getElementById("mySidenav2").style.width = "0";
+    document.getElementById("main").style.marginRight = "0";
+    document.getElementById('shoppingLstNavBar').style.display = '';
+  }
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById('pantryLst').style.display = 'none';
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById('pantryLst').style.display = '';
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
